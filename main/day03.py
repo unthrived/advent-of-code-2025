20b5f78164654d32f911ee4041bd8a27c95f2c39
Jun <111136649+unthrived@users.noreply.github.com>
2025-12-03 13:32:55 +0100

    Update day03.py

--- main/day03.py
+++ main/day03.py
@@ -1,21 +1,8 @@
 from utils import read_txt
 
 data = read_txt('data/day03.txt', numbers=False)
-print(data)
+# print(data)
 n = len(data)
-# brute force part 1
-# total = 0
-# for i in range(n):
-#     m = len(data[i])
-#     highest = int(data[i][0]+data[i][1])
-#     for j in range(m):
-#         for k in range(j+1,m):
-#             current = int(data[i][j]+data[i][k])
-#             # print(current)
-#             if current > highest:
-#                 highest = current
-#     total += highest
-# print(total)
 
 thought_process = """
 Essentially we want to find the highest value from all the numbers, except 12
@@ -23,7 +10,7 @@ then choose the leftmost highest, and then do the same for the queue minus one.
 """
 def find_highest(string, queue):
     n = len(string)
-    string = string[:n-queue+1]
+    string = string[:n-queue+1] # +1 here is important to check one more because the queue is one less
     highest = 0
     for i in range(n-queue+1):
         if int(string[i]) > highest:
@@ -34,7 +21,7 @@ total = 0
 for row in range(n):
     a = data[row] 
     result = ''
-    for i in reversed(range(1,13)):
+    for i in reversed(range(1,13)): # part a is changing 13 for 3
         index = find_highest(a, i)
         result += a[index]
         a = a[index+1:]
