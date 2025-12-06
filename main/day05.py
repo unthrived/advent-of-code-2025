d617720133da78b8050966432763d7012828bfce
Jun <111136649+unthrived@users.noreply.github.com>
2025-12-06 13:04:59 +0100

    Update day05.py

--- main/day05.py
+++ main/day05.py
@@ -1,7 +1,6 @@
 from utils import read_txt
 
 data = read_txt('data/day05.txt', numbers=False)
-print(data)
 n = len(data)
 for i in range(n):
     if data[i] == '':
@@ -11,7 +10,6 @@ for i in range(n):
 
 for i in range(len(data)):
     data[i] = data[i].split('-')
-print(data)
 count = 0
 for k in range(len(ids)): 
     for i in range(len(data)):
@@ -21,44 +19,45 @@ for k in range(len(ids)):
             break
 print('p1', count)
 
-print(data)
 for i in range(len(data)):
     data[i][0] = int(data[i][0])
     data[i][1] = int(data[i][1])
-final = [data[0]]
-print(final)
-for i in range(1, len(data)):
-    print('###', data[i], final)
-    found_left = False
-    found_right = False
-    for j in range(len(final)):
-        found_left = False
-        found_right = False
-        if data[i][0] in range(final[j][0], final[j][1]+1):
-            found_left = True
-            print('left', final, data[i])
-            for k in range(len(final)):
-                if data[i][1] in range(final[k][0], final[k][1]+1):
-                    print('right', k)
-                    found_right = True
-                    if j != k:
-                        final[j][1] = final[k][1]
-                        final[k][0] = -1
-                        final[k][1] = 0
-                        break
-                if not found_right:
-                    final[j][1] = data[i][1]
-        elif data[i][1] in range(final[j][0], final[j][1]+1):
-            found_right = True
-            print('right',final, data[i])
-            for k in range(len(final)):
-                if data[i][0] in range(final[k][0], final[k][1]+1):
-                    found_left = True
-                    final[j][0] = data[k][0]
-                    final[k][0] = -1
-                    final[k][1] = 0
-                if not found_left:
-                    final[j][0] = data[i][0]
-    if not found_right and not found_left:
-        final.append(data[i])
-print(final)
+for i in range(len(data)):
+    for j in range(len(data)):
+        if i!=j:
+            if data[i][0] in range(data[j][0], data[j][1]):
+                data[i][0] = data[j][1]
+            if data[i][1] in range(data[j][0], data[j][1]):
+                data[i][1] = data[j][0]
+total = 0
+active = [0 for i in range(len(data))]
+for i in range(len(data)):
+    if data[i][1] >= data[i][0]:
+        active[i] = 1
+print(active)
+print(data)
+changed = True
+while changed:
+    changed = False
+    for i in range(len(data)):
+        if active[i]:
+            for j in range(len(data)):
+                if active[j]:
+                    if i != j:
+                        if data[i][0] == data[j][1]:
+                            data[i][0] = data[j][0]
+                            active[j] = 0
+                            changed = True
+                        if data[i][1] == data[j][0]:
+                            data[i][1] = data[j][1]
+                            active[j] = 0
+                            changed = True
+total = 0
+for i in range(len(active)):
+    if active[i]:
+        total += data[i][1] - data[i][0]
+        total += 1
+print(active)
+print(data)
+print(total) # 353716783056988 low
+             # 353716783056994
